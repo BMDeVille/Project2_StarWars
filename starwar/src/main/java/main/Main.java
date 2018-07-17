1a737258067b1488ae2f93f3a4a15f7d18030f9d
Brandon DeVille <bmdnt4@mst.edu>
2018-07-17 08:42:08 -0400

    Jenkins test 5

--- starwar/src/main/java/main/Main.java
+++ starwar/src/main/java/main/Main.java
@@ -9,6 +9,7 @@ import models.User;
 
 public class Main {
 	public static void main(String[] args) {
+		// Jenkins test 5
 		DaoService ds = new DaoServiceImpl();
 		Date date = new Date();
 		User u = new User("test1","l1","f1","test1@test1.com","test1",new Timestamp(date.getTime()),"secans",1);
