6130e55010ab0e9cb9a1ba1a6d780321cf727e2a
Jeffrey Martinez <jeffreymartinez808@gmail.com>
2018-07-17 20:08:20 -0400

    'still more feed stuff'

--- empire/src/app/feed/feed.component.ts
+++ empire/src/app/feed/feed.component.ts
@@ -4,6 +4,7 @@ import { ModalService } from '../services/modal.service';
 import { LoginComponent } from '../login/login.component';
 import { PostService } from '../services/post.service';
 import { IPost } from '../db_models/post';
+import { IComment } from '../db_models/comment';
 
 @Component({
   selector: 'app-feed',
@@ -13,8 +14,11 @@ import { IPost } from '../db_models/post';
 export class FeedComponent implements OnInit, AfterViewInit {
 
   posts: IPost[] = [];
-  constructor(private _modalService: ModalService, _postservice: PostService) { 
+  cp: IPost;
+  toggleFlag: boolean;
+  constructor(private _modalService: ModalService, _postservice: PostService) {
     this.posts = _postservice.getFeed('');
+    this.toggleFlag = false;
   }
 
   ngOnInit() {
@@ -29,5 +33,34 @@ export class FeedComponent implements OnInit, AfterViewInit {
     };
     this._modalService.init(LoginComponent, inputs, {});
   }
+  toggleComments(event: { target: HTMLInputElement; }) {
+    if (!this.toggleFlag) {
+    const commentId = +event.target.parentElement.id; // + casts to number
+    for (let i = 0; i < this.posts.length; ++i) {
+        if (this.posts[i].pid === commentId) {
+          this.cp = this.posts[i];
+          break;
+        }
+    }
+    const ul = document.createElement('ul');
+    ul.className = 'feed';
+    for (let i = 0; i < this.cp.comments.length; ++i) {
+      const li = document.createElement('li');
+      li.innerHTML = this.cp.comments[i].body
+       + '<button style="border: none; background-color: black" (click)="likeComment">'
+       + '<img src = "assets/images/1485477009-like_78561.png" width = "30px" height = "30px"></button>['
+        + (this.cp.comments[i].likes != null ? this.cp.comments[i].likes.length : 0 ) + ']';
+        ul.appendChild(li);
+    }
+    event.target.parentElement.parentElement.appendChild(ul);
+  } else {
+    event.target.parentElement.parentElement.removeChild(event.target.parentElement.nextSibling);
+  }
+  this.toggleFlag = !this.toggleFlag;
+  }
 
+  likeComment(c: IComment) {
+    c.likes.push({'id': 1, 'fname': 'Darth', 'lname': 'Vador', 'username': 'SithLord', 'about': '',
+    'sec_ans': '', 'dob': new Date(), 'allegiance': 1, 'email': 'd.vador@empire.gov', 'followers': null, 'posts': null, 'image': 1});
+  }
 }
--- empire/src/app/services/post.service.ts
+++ empire/src/app/services/post.service.ts
@@ -7,15 +7,15 @@ import { IComment } from '../db_models/comment';
   providedIn: 'root'
 })
 export class PostService {
-    comment: IComment;
+    comment: IComment[];
     posts: IPost[] = [];
     user: IUser;
   constructor() {
-    this.comment = {'cid': 1, 'body': '... we have cookies.', 'likes': null};
+    this.comment = [{'cid': 1, 'body': '... we have cookies.', 'likes': null}, {'cid': 2, 'body': 'I LOVE cookies!', 'likes': null}];
     this.user = {'id': 1, 'fname': 'Darth', 'lname': 'Vador', 'username': 'SithLord', 'about': '',
      'sec_ans': '', 'dob': new Date(), 'allegiance': 1, 'email': 'd.vador@empire.gov', 'followers': null, 'posts': null, 'image': 1};
     this.posts = [{'pid': 1, 'body': 'Welcome to the Empire', 'youtube': '', 'created': new Date() ,
-     'comments': null, 'likes': [this.user], 'images': null},
+     'comments': this.comment, 'likes': [this.user], 'images': null},
     {'pid': 2, 'body': 'Test post', 'youtube': '', 'created': new Date() , 'comments': null, 'likes': null, 'images': null}
   ];
   }
