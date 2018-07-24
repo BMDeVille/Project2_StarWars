9d0a9e906e643b4b9803c2946ccd0ab04c441867
Jeffrey Martinez <jeffreymartinez808@gmail.com>
2018-07-24 14:51:59 -0400

    no changes just looking at the files

--- starwar/src/main/java/com/p2/main/Main.java
+++ starwar/src/main/java/com/p2/main/Main.java
@@ -1,12 +1,19 @@
 package com.p2.main;
 
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Component;
 
 import com.p2.dao.DaoService;
+import com.p2.models.Allegiance;
 import com.p2.models.SecurityQ;
+import com.p2.models.User;
 
 @Component
 public class Main {
@@ -17,6 +24,7 @@ public class Main {
 	public static void main(String[] args) {
 		Main m = new Main();
 		m.initializeSecQues();
+		
 	}
 	
 	public void initializeSecQues() {
@@ -28,4 +36,5 @@ public class Main {
 		ds.insertSecQues(new SecurityQ(1, "Academy you attended as a youngling."));
 		ds.insertSecQues(new SecurityQ(1, "Name of your first mentor."));
 	}
+	
 }
