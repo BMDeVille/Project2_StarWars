d34f3fedac9529fba6a6f492e5fe792f6ed97a93
Brandon DeVille <bmdnt4@mst.edu>
2018-07-26 18:54:11 -0400

    Fixed database issues

--- starwar/src/main/java/com/p2/main/Main.java
+++ starwar/src/main/java/com/p2/main/Main.java
@@ -19,22 +19,10 @@ import com.p2.models.User;
 @Component
 public class Main {
 	public static ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-	@Autowired
-	DaoService ds;
 	
 	public static void main(String[] args) {
-		Main m = new Main();
-		m.initializeSecQues();
-	}
-	
-	public void initializeSecQues() {
-		ds.insertSecQues(new SecurityQ(1, "Name of your favorite star ship."));
-		ds.insertSecQues(new SecurityQ(1, "Name of your first acquired droid."));
-		ds.insertSecQues(new SecurityQ(1, "Planet your parents met on."));
-		ds.insertSecQues(new SecurityQ(1, "Your favorite meal."));
-		ds.insertSecQues(new SecurityQ(1, "Mother's maiden name."));
-		ds.insertSecQues(new SecurityQ(1, "Academy you attended as a youngling."));
-		ds.insertSecQues(new SecurityQ(1, "Name of your first mentor."));
+		DaoService ds = context.getBean("DaoService", DaoService.class);
+		System.out.println(ds.selectAll());
 	}
 	
 }
